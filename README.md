d6c9fc745ad1e5b83fdaacd149f6d6551a110583
mvpratt <24981541+mvpratt@users.noreply.github.com>
2022-12-06 11:48:01 -0500

    get macaroon and node url from env

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
 
-Set these environment variables
+## Set these environment variables
 
+'''
+export LN_NODE_URL=xxxx
 export MACAROON_HEADER=xxxx
 
 export SMS_ENABLE=TRUE
@@ -9,4 +11,6 @@ export TWILIO_ACCOUNT_SID=xxxxxxxxx
 export TWILIO_AUTH_TOKEN=xxxxxxxxx
 export TWILIO_PHONE_NUMBER=xxxxxxxxx
 export TO_PHONE_NUMBER=xxxxxxxxx
+''
 
+Set SMS_ENABLE to 'FALSE' to disable text message updates
--- nw.go
+++ nw.go
@@ -14,8 +14,8 @@ import (
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-// request to REST api endpoint
-func call(url, method string) (http.Response, error) {
+// HTTP request to lightning node - requires macaroon for authentication
+func http_node_request(url, method string, macaroon string) (http.Response, error) {
 	client := &http.Client{
 		Timeout: time.Second * 10,
 	}
@@ -24,8 +24,7 @@ func call(url, method string) (http.Response, error) {
 		return *req.Response, fmt.Errorf("Got error %s", err.Error())
 	}
 
-	// set credentials to access node
-	macaroon := os.Getenv("MACAROON_HEADER")
+	// Set credentials to access lightning node
 	req.Header.Set("grpc-metadata-macaroon", macaroon)
 	req.Header.Set("user-agent", "nodewatcher")
 
@@ -37,7 +36,7 @@ func call(url, method string) (http.Response, error) {
 	return *response, nil
 }
 
-// send a text message
+// Send a text message
 func send_text(twilio_client *twilio.RestClient, msg string, to string, from string) {
 	params := &openapi.CreateMessageParams{}
 	params.SetTo(to)
@@ -48,7 +47,7 @@ func send_text(twilio_client *twilio.RestClient, msg string, to string, from str
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
-		fmt.Println("SMS sent successfully!")
+		fmt.Println("\nSMS sent successfully!")
 	}
 }
 
@@ -56,24 +55,23 @@ func main() {
 
 	fmt.Println("\n Getting node status ...")
 
-	// get environment variables
+	// Get environment variables
 	sms_enable := os.Getenv("SMS_ENABLE")
 	sms_to := os.Getenv("TO_PHONE_NUMBER")
 	sms_from := os.Getenv("TWILIO_PHONE_NUMBER")
 	twilio_account_sid := os.Getenv("TWILIO_ACCOUNT_SID")
 	twilio_auth_token := os.Getenv("TWILIO_AUTH_TOKEN")
-	node_url := os.Getenv("LN_NODE_URL")
 
 	// Note: Twilio credentials must be defined as environment variables for text messaging to work.
 	if sms_enable != "TRUE" {
-		fmt.Println("\nWARNING: Text messages disabled" +
-			"set environment variable SMS_ENABLE to TRUE to enable SMS status updates")
+		fmt.Println("\nWARNING: Text messages disabled. " +
+			"Set environment variable SMS_ENABLE to TRUE to enable SMS status updates")
 	} else if sms_enable == "TRUE" && (twilio_account_sid == "" || twilio_auth_token == "") {
-		fmt.Println("\nERROR: Twilio credentials not set." +
+		fmt.Println("\nERROR: Twilio credentials not set. " +
 			"TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN must be set as environment variables")
 		return
 	} else if sms_to == "" || sms_from == "" {
-		fmt.Println("\nERROR: Twilio phone numbers not set." +
+		fmt.Println("\nERROR: Twilio phone numbers not set. " +
 			"TWILIO_PHONE_NUMBER and TO_PHONE_NUMBER must be set as environment variables")
 		return
 	}
@@ -81,8 +79,20 @@ func main() {
 	// Twilio client
 	twilio_client := twilio.NewRestClient()
 
-	// request status from the node
-	response, err := call(node_url, "GET")
+	// Get lightning node credentials
+	node_url := os.Getenv("LN_NODE_URL")
+	macaroon := os.Getenv("MACAROON_HEADER")
+	if node_url == "" {
+		fmt.Println("\nERROR: LN_NODE_URL environment variable must be set.")
+		return
+
+	} else if macaroon == "" {
+		fmt.Println("\nERROR: MACAROON_HEADER environment variable must be set.")
+		return
+	}
+
+	// Request status from the node
+	response, err := http_node_request(node_url, "GET", macaroon)
 	if err != nil {
 		print(err)
 	}
@@ -102,22 +112,23 @@ func main() {
 		print(err2)
 	}
 
-	// marshall data into JSON
-	status_json, err := json.MarshalIndent(data, " ", "    ")
-	if err != nil {
-		log.Fatalf(err.Error())
+	// Marshall data into JSON
+	status_json, err3 := json.MarshalIndent(data, " ", "    ")
+	if err3 != nil {
+		log.Fatalf(err3.Error())
 	}
+	var status_string string
+	status_string = string(status_json)
 
-	// check how long since last block
-	// convert unix time string into base10, 64-bit int
-	lastblktime, err := strconv.ParseInt(data.BestHeaderTimestamp, 10, 64)
+	// Check how long since last block. Convert unix time string into base10, 64-bit int
+	lastblktime, _ := strconv.ParseInt(data.BestHeaderTimestamp, 10, 64)
 	t := time.Unix(lastblktime, 0)
 	timesincelastblk := time.Now().Sub(t)
 
-	var status_string string
-	status_string = string(status_json)
+	// Contents to be sent via SMS
 	var text_msg string
-	// detect if lightning node believes it is synced
+
+	// Detect if lightning node is synced
 	if data.SyncedToChain != true {
 		text_msg = fmt.Sprintf("WARNING: Lightning node is not fully synced."+
 			"\nDetails: %s", status_string)
@@ -130,9 +141,10 @@ func main() {
 				"\nLast block received %s minutes ago", data.Alias, timesincelastblk)
 	}
 
-	// send SMS with status
+	// Send SMS with status
 	if sms_enable == "TRUE" {
 		send_text(twilio_client, text_msg, sms_to, sms_from)
 	}
+
 	fmt.Println(text_msg)
 }
