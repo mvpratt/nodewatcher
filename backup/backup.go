2f7e7512472de35322899e6b1911d51e2aa84ddb
mvpratt <24981541+mvpratt@users.noreply.github.com>
2023-01-31 14:39:12 -0700

    WIP - subscribe to channel backups

--- backup/backup.go
+++ backup/backup.go
@@ -3,8 +3,8 @@ package backup
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/lightningnetwork/lnd/lnrpc"
@@ -14,44 +14,95 @@ import (
 
 // SaveChannelBackups ...
 func SaveChannelBackups(statusPollInterval time.Duration, node *db.Node, client lnrpc.LightningClient, depotDB *bun.DB) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(5)*time.Second)
+	defer cancel()
+
+	// todo - does ctx need macaroon auth?
+	backupStream, err := client.SubscribeChannelBackups(ctx, &lnrpc.ChannelBackupSubscription{})
+	if err != nil {
+		log.Print(err.Error())
+	}
+
+	var wg sync.WaitGroup
+	backupUpdates := make(chan *lnrpc.ChanBackupSnapshot)
+	streamErr := make(chan error, 1)
+
+	// Backups updates goroutine.
+	wg.Add(1)
+	go func() {
+		log.Println("getting snapshots from stream ...")
+		defer wg.Done()
+		for {
+			snapshot, err2 := backupStream.Recv() // seems like this is timing out  - maybe exeed deadline?
+			if err != nil {
+				log.Print("error getting snapshot")
+				log.Print(err2.Error())
+				streamErr <- err2
+				return
+			}
+
+			select {
+			case backupUpdates <- snapshot:
+			case <-ctx.Done():
+				log.Print("ctx.Done()")
+				return
+			}
+		}
+	}()
+
+	go func() {
+		log.Println("waiting for snaps ...")
+		for {
+			snap, ok := <-backupUpdates
+			log.Printf("got a snap %#v, ok:%t", snap, ok)
+		}
+	}()
+
+	//go routine
+	//go log.Printf("\ngrpc stream:\n%v\n", backupStream)
+
 	for {
-		fmt.Println("\nSaving channel backups ...")
-		response, err := GetChannels(client)
-		if err != nil {
-			log.Print(err.Error())
-		}
-		for _, item := range response.Channels {
-			err := db.InsertChannel(item, node.Pubkey, depotDB)
-			if err != nil {
-				log.Print(err.Error())
-			}
-		}
+		log.Println("\nSaving channel backups ...")
+		// 	response, err := GetChannels(client)
+		// 	if err != nil {
+		// 		log.Print(err.Error())
+		// 	}
+		// 	for _, item := range response.Channels {
+		// 		err := db.InsertChannel(item, node.Pubkey, depotDB)
+		// 		if err != nil {
+		// 			log.Print(err.Error())
+		// 		}
+		// 	}
 
-		// static channel backup
-		chanBackups, err := GetChannelBackups(client)
-		if err != nil {
-			log.Print(err.Error())
-		}
+		// 	// static channel backup
+		// 	chanBackups, err := GetChannelBackups(client)
+		// 	if err != nil {
+		// 		log.Print(err.Error())
+		// 	}
 
-		for _, item := range chanBackups.SingleChanBackups.ChanBackups {
-			err := db.InsertChannelBackup(item, depotDB)
-			if err != nil {
-				log.Print(err.Error())
-			}
-		}
+		// 	for _, item := range chanBackups.SingleChanBackups.ChanBackups {
+		// 		err := db.InsertChannelBackup(item, depotDB)
+		// 		if err != nil {
+		// 			log.Print(err.Error())
+		// 		}
+		// 	}
 
-		// mulitchannel backup
-		err = db.InsertMultiChannelBackup(chanBackups.MultiChanBackup, node.Pubkey, depotDB)
-		if err != nil {
-			log.Print(err.Error())
-		}
+		// 	// multichannel backup
+		// 	err = db.InsertMultiChannelBackup(chanBackups.MultiChanBackup, node.Pubkey, depotDB)
+		// 	if err != nil {
+		// 		log.Print(err.Error())
+		// 	}
 
-		// WIP
-		// get backup from db
-		// multiBackup, err := db.FindMultiChannelBackupByPubkey(node.Pubkey, depotDB)
-		// if err != nil {
-		// 	log.Print(err.Error())
-		// }
+		// 	// todo - testing
+		// 	multiBackup, err := db.FindMultiChannelBackupByPubkey(node.Pubkey, depotDB)
+		// 	if err != nil {
+		// 		log.Print(err.Error())
+		// 	}
+		// 	log.Printf("\n\nmulti backup: %v", multiBackup)
+
+		// 	// todo .. add single channel backups from the db?
+		// 	// resp, err := VerifyBackup(client, multiBackup)
+		// 	// log.Printf("\n\nverify backup response: %v", resp)
 
 		time.Sleep(statusPollInterval * time.Second)
 	}
@@ -80,8 +131,20 @@ func GetChannelBackups(client lnrpc.LightningClient) (*lnrpc.ChanBackupSnapshot,
 }
 
 // VerifyBackup ...
-func VerifyBackup(client lnrpc.LightningClient, snapshot lnrpc.ChanBackupSnapshot) (*lnrpc.VerifyChanBackupResponse, error) {
+func VerifyBackup(client lnrpc.LightningClient, snapshot *lnrpc.ChanBackupSnapshot) (*lnrpc.VerifyChanBackupResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	return client.VerifyChanBackup(ctx, &snapshot)
+	return client.VerifyChanBackup(ctx, snapshot)
 }
+
+// SubscribeChannelBackups gets new backup whenever channel state changes
+// func SubscribeChannelBackups(client lnrpc.LightningClient) (lnrpc.Lightning_SubscribeChannelBackupsClient, error) {
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+// 	defer cancel()
+
+// 	//var sub lnrpc.ChannelBackupSubscription
+// 	//var stream lnrpc.Lightning_SubscribeChannelBackupsClient
+
+// 	stream, err := client.SubscribeChannelBackups(ctx, &lnrpc.ChannelBackupSubscription{})
+// 	return stream, err
+// }
