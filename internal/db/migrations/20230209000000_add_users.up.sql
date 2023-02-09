3cd4137dfb6b5b88a1d2404dd1af8e50e20513e4
mvpratt <24981541+mvpratt@users.noreply.github.com>
2023-02-09 10:00:39 -0700

    add db queries

---
+++ internal/db/migrations/20230209000000_add_users.up.sql
@@ -0,0 +1,12 @@
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+
+--migration:split
+CREATE TABLE "public"."users" (
+    "id" int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    "email" varchar,
+    "password" varchar,
+    PRIMARY KEY ("id")
+);
+
+--migration:split
+ALTER TABLE "users" ADD CONSTRAINT unique_user_name UNIQUE ("email");
