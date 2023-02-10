f2c72a6c46000e0f877c122f2e63d529db03cbae
mvpratt <24981541+mvpratt@users.noreply.github.com>
2023-02-10 09:26:45 -0700

    return err if node already exists

--- internal/controllers/nodecontroller.go
+++ internal/controllers/nodecontroller.go
@@ -43,14 +43,26 @@ func GetNodes(context *gin.Context) {
 		"pubkey":   node.Pubkey})
 }
 
+// CreateNode adds a node to the database. If a node with the same pubkey already
+// exists, an error is returned
 func CreateNode(context *gin.Context) {
-	var node = db.Node{ID: 0}
+	var node = db.Node{}
 	if err := context.ShouldBindJSON(&node); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		context.Abort()
 		return
 	}
+
+	exists, _ := db.FindNodeByPubkey(node.Pubkey)
+	if exists.Pubkey == node.Pubkey {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "node already exists"})
+		context.Abort()
+		return
+	}
+
+	node.ID = 0 // set to 0 so the db will auto-increment
 	err := db.InsertNode(&node)
+
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		context.Abort()
