32a5d7bc06690199643d57c7b621f9d316708d9c
mvpratt <24981541+mvpratt@users.noreply.github.com>
2023-03-13 13:55:06 -0600

    fix - connect to local db in example env

--- env-example.sh
+++ env-example.sh
@@ -6,7 +6,7 @@ export TWILIO_AUTH_TOKEN=BEEF42
 export TWILIO_PHONE_NUMBER=+15556667777
 
 # database credentials
-export POSTGRES_HOST=nodewatcher-db
+export POSTGRES_HOST=localhost
 export POSTGRES_DB=postgres
 export POSTGRES_PORT=5432
 export POSTGRES_USER=nodewatcher
